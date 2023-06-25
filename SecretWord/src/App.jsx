69a362f9a670c14f0c592f4d9d1e63b43c7bf28d
Juliana Nunes Almeida Couto <juliana.couto@t2mlab.com>
2023-06-25 00:37:41 -0300

    Escolhendo palavra e categoria, tratando lowercase e acento

--- SecretWord/src/App.jsx
+++ SecretWord/src/App.jsx
@@ -19,9 +19,38 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+ 
+ const pickWordAndCategory = () => {
+  // escolhendo categoria aleatória.
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];// floor arredonda para baixo no caso de estar recebendo um float
+
+  // escolhendo palavra aleatória.
+     const word =
+     words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(category, word);
+
+    return {word, category}
+    }
 
   //Iniciando o jogo.
   const startGame = () =>{
+
+    // Escolhendo uma palavra
+    const {word, category} = pickWordAndCategory();
+
+    // Separando a palavra em letras e tratando Maiúscula
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ''));
+
+    console.log(category, word);
+    console.log(wordLetters);
     setGameStage(stages[1].name)
   }
 
@@ -34,7 +63,7 @@ function App() {
   const retry = () =>{
     setGameStage(stages[0].name)
   }
-  
+
   return (
     <div className="App">
      {gameStage === "start" && <TelaInicial startGame={startGame}/>}
--- SecretWord/src/data/words.jsx
+++ SecretWord/src/data/words.jsx
@@ -1,7 +1,7 @@
 export const wordsList = {
-    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena","Painel"],
-    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena","Painel","Vidro"],
+    fruta: ["Ameixa","Banana", "Caqui", "Damasco", "Framboesa", "Goiaba", "Jabuticaba","Kiwi", "Laranja","Limao", "Maçã","Mamão","Manga","Nectarina", "Pera", "Tangerina", "Uva"],
+    corpo: ["Braço", "Cérebro", "Esqueleto","Joelho", "Lingua", "Perna",  "Pescoço", "Olhos"],
     computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
     programação: ["Linguagem", "Framework", "JavaScript", "React"],
     alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
