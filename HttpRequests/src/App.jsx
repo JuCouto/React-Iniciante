3b049ca71600dd37186185b737407d9f4a3658e7
Ju Couto <jnalmeida@ymail.com>
2023-07-03 20:54:29 -0300

    Método POST v1

--- HttpRequests/src/App.jsx
+++ HttpRequests/src/App.jsx
@@ -8,7 +8,11 @@ const url= "http://localhost:3000/products";
 function App() {
   const [products, setProducts] = useState([])
 
-  useEffect(() =>{
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  useEffect( () =>{
     async function fetchData() {
     const res = await fetch(url)
 
@@ -18,10 +22,29 @@ function App() {
     // armazena no set
     setProducts(data);
     }
+    
     fetchData();
 
   },[]);
+console.log(name,price)
 
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const product = {
+    name,
+    price,
+  };
+
+  const res = await fetch (url, {
+    method:"Post",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body: JSON.stringify(product),
+  });
+  console.log(product)
+};
 
   return (
     
@@ -33,9 +56,34 @@ function App() {
             {product.name} - R$: {product.price}</li>
         ))}
       </ul>
+
+      <div className="add-product">
+        <p>Adicionar produto:</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+         <input type="submit" value="Criar" />
+        </form>
+      </div>
       </div> 
     
   )
 }
 
-export default App
+export default App;
